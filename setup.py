ada5853df57fb053e4dfa72f5c60d714d6105208
Denis BOURGE <denis.bourge@sharingcloud.com>
2021-11-14 14:25:51 +0100

    wip

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
-import setuptools
+from distutils.core import setup
 
-setuptools.setup()
+from vagrant_ansible_provisioner.version import __version__
+
+setup(
+    name="vagrant-ansible-provisioner",
+    version=__version__,
+    packages=[
+        "vagrant_ansible_provisioner",
+        "vagrant_ansible_provisioner.commands",
+    ]
+)
