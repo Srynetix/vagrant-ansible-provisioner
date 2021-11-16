8aae8a5a8e2db238938b287a206982fb636b292d
Denis BOURGE <denis.bourge@sharingcloud.com>
2021-11-16 12:20:38 +0100

    Missing directives in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
 setup(
+    package_data={"vagrant_ansible_provisioner": ["py.typed"]},
     packages=[
         "vagrant_ansible_provisioner",
         "vagrant_ansible_provisioner.commands",
-    ]
+    ],
+    zip_safe=False
 )
