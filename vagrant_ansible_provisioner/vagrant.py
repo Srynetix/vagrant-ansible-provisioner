7e2b764ba0724c0f110c9c00734171297d71ce82
Denis BOURGE <denis.bourge@sharingcloud.com>
2021-11-16 10:39:53 +0100

    Add more vagrant methods

--- vagrant_ansible_provisioner/vagrant.py
+++ vagrant_ansible_provisioner/vagrant.py
@@ -1,8 +1,9 @@
 import hashlib
 import os
+import subprocess
 from typing import Any, Dict, Optional
 
-from .utils import exec_or_bail, print_info
+from .utils import bail, exec_or_bail, print_info
 
 INSECURE_PUBLIC_KEY = """
 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key
@@ -46,3 +47,23 @@ def generate_box_manifest(
             }
         ],
     }
+
+
+def upload_file_to_guest(src: str, dest: str, *, vm_name: Optional[str] = None, verbose: bool = False) -> None:
+    cmd = f'vagrant upload "{src}" "{dest}"'
+    if vm_name is not None:
+        cmd = f'{cmd} "{vm_name}"'
+    exec_or_bail(cmd, verbose=verbose)
+    print("")
+
+
+def exec_on_guest(cmd: str, *, verbose: bool = False, bail_on_error: bool = True) -> int:
+    if verbose:
+        print_info(f"Executing command '{cmd}' ...\n")
+
+    code = subprocess.call(["vagrant", "ssh", "-c", cmd])
+
+    if bail_on_error and code != 0:
+        bail()
+
+    return code
